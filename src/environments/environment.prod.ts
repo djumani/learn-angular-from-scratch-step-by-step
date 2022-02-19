5d1f5967c2395414e72a9efce69488b4afcb8192
Deepa Jumani <deepa.jumani@gmail.com>
2022-02-19 18:15:16 -0500

    'learn-angular-from-scratch-step-by-step'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://pu7iy2mux1.execute-api.us-east-1.amazonaws.com'
 };
